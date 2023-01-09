c008962a29d941ba430ac005c7b669431ccd843d
Denilson Barahona <denilson.barahona@dd3tech.mx>
2023-01-09 17:10:26 -0600

    add/cors

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
+import cors from "cors";
 import notesRouter from "@notes/routes/notes.routes";
 
 const app: Express = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/notes', notesRouter);
 
